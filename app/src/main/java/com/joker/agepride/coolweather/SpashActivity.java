6a7b8f549ed235a137ff945a75399bb6d81ee385
legend奇 <919625271@qq.com>
2017-11-17 15:26:04 +0800

    First commit

---
+++ app/src/main/java/com/joker/agepride/coolweather/SpashActivity.java
@@ -0,0 +1,13 @@
+package com.joker.agepride.coolweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spash);
+    }
+}
